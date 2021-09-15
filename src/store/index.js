e7b7c1d5210c8a6fed60aed280421e2f1895c1c5
Anthony Dionise <anthonydionise@Anthonys-MacBook-Pro.local>
2021-09-15 12:17:28 -0400

    fetch tokens

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,35 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    access_token: "",
+    refresh_token: "",
+  },
+  getters: {
+    access_token: (state) => {
+      return state.access_token;
+    },
+    refresh_token: (state) => {
+      return state.refresh_token;
+    },
+  },
+  mutations: {
+    set_access_token(state, access_token) {
+      // mutate state
+      state.access_token = access_token;
+    },
+    set_refresh_token(state, refresh_token) {
+      // mutate state
+      state.refresh_token = refresh_token;
+    },
+  },
+  actions: {
+    set_access_token(context, access_token) {
+      context.commit("set_access_token", access_token);
+    },
+    set_refresh_token(context, refresh_token) {
+      context.commit("set_refresh_token", refresh_token);
+    }
+  },
   modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="start">start</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { mapGetters } from "vuex";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  computed: {
+    ...mapGetters(["access_token", "refresh_token"]),
+  },
+  mounted() {
+    if (this.access_token && this.refresh_token) {
+      console.log(this.access_token);
+      console.log('refresh: ', this.refresh_token);
+    } else if (this.$route.query.access_token && this.$route.query.refresh_token) {
+      this.$store.dispatch("set_refresh_token", this.$route.query.refresh_token);
+      this.$store.dispatch("set_access_token", this.$route.query.access_token);
+    }
   },
 };
 </script>
