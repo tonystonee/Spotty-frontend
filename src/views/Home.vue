7f46710f2112c2dcf35bedaed3191159f69168bd
Anthony Dionise <anthonydionise@Anthonys-MacBook-Pro.local>
2021-09-17 13:45:17 -0400

    CORS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="home">
     <button @click="start">start</button>
+
+    <p v-if="status !== 200">{{ statusText }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 import { mapGetters } from "vuex";
 export default {
   name: "Home",
+  data() {
+    return {
+      status: 200,
+      statusText: "",
+    };
+  },
   computed: {
     ...mapGetters(["access_token", "refresh_token"]),
+  },
+  methods: {
+    async $_get_playback_state() {
+      let pkg;
+      try {
+        pkg = await axios.get("http://spotty-env.eba-hiwn3fbm.us-east-1.elasticbeanstalk.com/player?access_token=" + this.access_token, {
+            crossDomain: true,
+        });
+      } catch (xhr) {
+        console.log('uh oh!')
+        console.error(xhr);
+        return;
+      }
+      this.status = pkg.status;
+      this.statusText = pkg.statusText;
+      console.log('adfd')
+      console.log(pkg); 
+    },
+    start() {
+      this.$_get_playback_state();
+    }
   },
   mounted() {
     if (this.access_token && this.refresh_token) {
