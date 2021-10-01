c8d04b40ffbbc8e2316f0df6277e9962d30741c8
Anthony Dionise <anthonydionise@Anthonys-MacBook-Pro.local>
2021-10-01 13:00:26 -0400

    next, previous routes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <router-view />
+  <n-config-provider :theme="darkTheme" height="100%">
+    <n-layout>
+      <router-view></router-view>
+    </n-layout>
+  </n-config-provider>
 </template>
 
-<style lang="scss">
+<script>
+import { defineComponent, ref } from "vue";
+import { NConfigProvider, NLayout } from "naive-ui";
+import { darkTheme } from "naive-ui";
+
+export default defineComponent({
+  components: {
+    NConfigProvider,
+    NLayout,
+  },
+  setup() {
+    return {
+      darkTheme,
+      theme: ref(null),
+    };
+  },
+});
+</script>
+
+<style>
+html,
+body {
+    height: 100%;
+    background: #101015;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home">
-    <button v-if="loggedIn" @click="start">start</button>
-    <button v-else @click="login ">Connect your Spotify</button>
-    <p v-if="status !== 200">{{ statusText }}</p>
+    <n-space justify="center" vertical>
+      <n-button size="large" v-if="playing" @click="stop">STOP</n-button>
+      <n-button size="large" v-if="loggedIn" @click="start">start</n-button>
+      <n-button size="large" v-else @click="login">Connect your Spotify</n-button>
+      <p>{{count}}</p>
+
+      <p v-if="status !== 200">{{ statusText }}</p>
+  </n-space>
   </div>
 </template>
 
@@ -10,14 +15,30 @@
 // @ is an alias to /src
 import axios from "axios";
 import { mapGetters } from "vuex";
+import { NButton, NSpace } from "naive-ui";
+
 export default {
   name: "Home",
+
+  components: {
+    NButton,
+    NSpace,
+  },
+  created() {
+    window.onbeforeunload = function (event) {
+      this.handler(event);
+    }
+  },
   data() {
     return {
       // host: "http://spotty-env.eba-hiwn3fbm.us-east-1.elasticbeanstalk.com",
+      count: 0,
       host: "http://localhost:3000",
+      seconds: 5,
       status: 200,
       statusText: "",
+      playing: false,
+      timer: null,
     };
   },
   computed: {
@@ -27,10 +48,37 @@ export default {
     },
   },
   methods: {
-    async $_get_playback_state() {
+    $_next: function () {
+      const host = this.host;
+      const access_token = this.access_token;
+      const self = this;
+      async function next() {
+        let pkg;
+        try {  
+          pkg = await axios.get(`${host}/player/next?access_token=${access_token}`,   {
+              crossDomain: true,
+          });
+        } catch (xhr) {
+          console.log('uh oh!')
+          console.error(xhr);
+          return;
+        }
+        this.status = pkg.status;
+        this.statusText = pkg.statusText;
+        self.count++;
+        console.log(pkg);
+      };
+
+      if (self.count === 2) {
+        clearInterval(this.timer);
+      } else {
+        this.timer = setInterval(next, this.seconds * 1000);
+      }
+    },
+    async $_previous() {
       let pkg;
       try {
-        pkg = await axios.get(`${this.host}/player?access_token=${this.access_token}`,   {
+        pkg = await axios.get(`${this.host}/player/previous?access_token=${this.access_token}`,   {
             crossDomain: true,
         });
       } catch (xhr) {
@@ -40,15 +88,19 @@ export default {
       }
       this.status = pkg.status;
       this.statusText = pkg.statusText;
-      console.log('adfd')
-      console.log(pkg); 
+      console.log(pkg);
     },
     login() {
       window.location.replace(`${this.host}/login`);
     },
     start() {
-      this.$_get_playback_state();
-    }
+      this.$_previous();
+      this.$_next();
+      this.playing = true;
+    },
+    stop() {
+      clearInterval(this.timer);
+    },
   },
   mounted() {
     if (this.access_token && this.refresh_token) {
@@ -58,6 +110,9 @@ export default {
       this.$store.dispatch("set_refresh_token", this.$route.query.refresh_token);
       this.$store.dispatch("set_access_token", this.$route.query.access_token);
     }
+  },
+  beforeUnmount() {
+    this.$_stop_player();
   },
 };
 </script>
